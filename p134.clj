a2a4861bfa5ddc68d081a678957931c1228f6b8f
LongHua Huang <longhua1984@gmail.com>
2011-11-08 11:34:19 +0800

    Updated comments to make it look better

--- p134.clj
+++ p134.clj
@@ -1,5 +1,6 @@
 ; A nil key
-; Write a function which, given a key and map, returns true iff the map contains an entry with that key and its value is nil.
+; Write a function which, given a key and map, returns true
+; if the map contains an entry with that key and its value is nil.
 ;
 (fn [k m]
   (and (contains? m k)
--- p65.clj
+++ p65.clj
@@ -1,8 +1,15 @@
 ; Black Box Testing
-; Clojure has many sequence types, which act in subtly different ways. The core functions typically convert them into a uniform "sequence" type and work with them that way, but it can be important to understand the behavioral and performance differences so that you know which kind is appropriate for your application.
+; Clojure has many sequence types, which act in subtly different ways.
+; The core functions typically convert them into a uniform "sequence"
+; type and work with them that way, but it can be important to
+; understand the behavioral and performance differences so that you
+; know which kind is appropriate for your application.
 ;
-; Write a function which takes a collection and returns one of :map, :set, :list, or :vector - describing the type of collection it was given.
-; You won't be allowed to inspect their class or use the built-in predicates like list? - the point is to poke at them and understand their behavior.
+; Write a function which takes a collection and returns one of :map,
+; :set, :list, or :vector - describing the type of collection it was given.
+; You won't be allowed to inspect their class or use the built-in
+; predicates like list? - the point is to poke at them and understand
+; their behavior.
 ;
 (fn [coll]
   (let [first-char (first (pr-str coll))]
